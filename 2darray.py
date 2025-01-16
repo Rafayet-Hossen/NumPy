c0a39a1f9ccfb0aa3b2ccc4424d04ba7058a4c30
Rafayet Hossen <hossenmdrafayet@gmail.com>
2025-01-16 23:05:24 +0600

    2D array using numpy

---
+++ 2darray.py
@@ -0,0 +1,9 @@
+import numpy as np
+ar2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(ar2d) #print the array
+print(f'Array is {ar2d.shape}') #row and col in 2d array
+print(f'Array is: {ar2d.ndim} D') #dimention of the array
+
+'''
+
+'''
